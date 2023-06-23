2428c15148c59b759b6175d45b89c0d6cb328671
dev-ashrafuzzaman <dev.ashrafuzzaman@gmail.com>
2023-06-24 01:26:35 +0600

    [DONE] Basic Route Setup

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet-async';
+
+const Home = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>HLA | Home</title>
+            </Helmet>
+            <h2>Home</h2>
+        </div>
+    );
+};
+
+export default Home;
