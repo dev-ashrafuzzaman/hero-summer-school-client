2785d745c996f4e0edc5914c3e9248bd3d46eb52
dev-ashrafuzzaman <dev.ashrafuzzaman@gmail.com>
2023-06-26 02:42:10 +0600

    [UPDATE] call section responsive

--- src/pages/Home/CallSection/CallSection.jsx
+++ src/pages/Home/CallSection/CallSection.jsx
@@ -1,14 +1,14 @@
-import { FaPhone } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
 
 
 const CallSection = () => {
     return (
-        <div className="bg-[#111827] my-32 mx-auto max-w-screen-xl px-20 py-10 rounded-2xl flex justify-between items-center">
+        <div className="bg-[#111827] md:my-32 my-10 md:mx-auto max-w-screen-xl md:px-20 px-2 md:py-10 py-2 mx-2 rounded-2xl flex justify-between items-center">
             <div>
-                <h3 className="text-4xl font-bold text-white">Call for any information regarding the course</h3>
-                <p className="text-2xl text-slate-400 font-semibold">8 am to 11 pm</p>
+                <h3 className="md:text-4xl font-bold text-white">Call for any information</h3>
+                <p className="md:text-2xl text-slate-400 font-semibold">8 am to 11 pm</p>
             </div>
-            <button className="btn main-color-bg text-white font-bold w-[120px]"><FaPhone></FaPhone>16999</button>
+            <button className="btn main-color-bg text-white font-bold w-[120px]"><FaPhoneAlt></FaPhoneAlt>16999</button>
         </div>
     );
 };
