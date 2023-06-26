ea314aac974450739fa5771f64ed1980e96fce63
dev-ashrafuzzaman <dev.ashrafuzzaman@gmail.com>
2023-06-26 13:16:09 +0600

    [DONE] Auth provider setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import {
 } from "react-router-dom";
 import { HelmetProvider } from 'react-helmet-async';
 import { router } from './Routes/Routes.jsx';
+import AuthProvider from './providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <HelmetProvider>
       <RouterProvider router={router}></RouterProvider>
     </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,76 @@
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import firebaseApp from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(firebaseApp);
+const googleProvider = new GoogleAuthProvider();
+
+
+const AuthProvider = ({children}) => {
+    const [user , setUser] = useState(null);
+    const [loading , setLoading] = useState(true);
+
+
+    // Social Login with Google
+    const googleSigninUser = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    //  Create User Auth with gmail and pass
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // Login User with email and pass
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // LogOut user auth
+    const logoutUser = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    // update Profile
+    const updateProfileUser = (name, photoURL) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photoURL
+        })
+    }
+
+    useEffect(() =>{
+        const unsubscribe =   onAuthStateChanged(auth , currentUser => {
+               setUser(currentUser);
+               console.log('current user' , currentUser);
+               setLoading(false)
+           });
+   
+           return () => {
+               return unsubscribe();
+           }
+       }, []);
+
+    const authDetails = {
+        user ,
+        loading,
+        googleSigninUser,
+        createUser,
+        loginUser,
+        logoutUser,
+        updateProfileUser
+    }
+
+
+    return (
+        <AuthContext.Provider value={authDetails}>
+            {children} 
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
